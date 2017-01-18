6f4983d7c62bfbf0b1f9010dc47ef7f558797173
Michał Brzuchalski <michal.brzuchalski@gmail.com>
2017-01-18 21:25:02 +0100

    Fixed tests

--- tests/FilesystemStreamWrapperTest.php
+++ tests/FilesystemStreamWrapperTest.php
@@ -51,63 +51,6 @@ class FilesystemStreamWrapperTest extends PHPUnit_Framework_TestCase
         $this->assertFalse(in_array('test2', stream_get_wrappers()));
     }
 
-//    public function testTouchRenameAndRemoveFile()
-//    {
-//        FilesystemStreamWrapper::register('test', $this->testRootPath);
-//
-//        $path = 'test://touch-and-remove.txt';
-//        $moved = 'test://touch-moved.txt';
-//        $this->assertFileNotExists($path);
-//        $this->assertTrue(touch($path));
-//        $this->assertFileExists($path);
-//        $this->assertTrue(rename($path, $moved));
-//        $this->assertFileNotExists($path);
-//        $this->assertFileExists($moved);
-//        $this->assertTrue(unlink($moved));
-//        $this->assertFileNotExists($moved);
-//
-//        FilesystemStreamWrapper::unregister('test');
-//    }
-//
-//    public function testMkdirMoveAndRmdir()
-//    {
-//        FilesystemStreamWrapper::register('test', $this->testRootPath);
-//
-//        $path = 'test://some-path/some-dir';
-//        $moved = 'test://some-path/new-dir';
-//        $this->assertDirectoryNotExists($path);
-//        $this->assertTrue(mkdir($path, 0777, true));
-//        $this->assertDirectoryExists($path);
-//        $this->assertTrue(rename($path, $moved));
-//        $this->assertDirectoryNotExists($path);
-//        $this->assertDirectoryExists($moved);
-//        $this->assertTrue(rmdir($moved));
-//        $this->assertDirectoryNotExists($moved);
-//
-//        FilesystemStreamWrapper::unregister('test');
-//    }
-//
-//    public function testDirectoryListing()
-//    {
-//        FilesystemStreamWrapper::register('test', $this->testRootPath);
-//
-//        touch('test://file1.txt');
-//        touch('test://file2.txt');
-//        mkdir('test://tmp');
-//        $directory = opendir('test://');
-//        $this->assertEquals('.', readdir($directory));
-//        $this->assertEquals('some-path', readdir($directory));
-//        $this->assertEquals('file1.txt', readdir($directory));
-//        $this->assertEquals('file2.txt', readdir($directory));
-//        $this->assertEquals('..', readdir($directory));
-//        $this->assertEquals('tmp', readdir($directory));
-//
-//        rewinddir($directory);
-//        $this->assertEquals('.', readdir($directory));
-//
-//        FilesystemStreamWrapper::unregister('test');
-//    }
-
     /**
      * Tests if the stream wrapper returns the expected result when reading a directory.
      */
@@ -120,7 +63,7 @@ class FilesystemStreamWrapperTest extends PHPUnit_Framework_TestCase
         while ($entry = readdir($dir)) {
             $entries[] = $entry;
         }
-        $this->assertSame(array('.', 'directory', '..', 'file.txt'), $entries);
+        $this->assertCount(0, array_diff(['.', 'directory', '..', 'file.txt'], $entries));
     }
 
     /**
