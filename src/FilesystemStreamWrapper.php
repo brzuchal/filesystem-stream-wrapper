1c43ddc45efe1e896467fa9fc95df06c47ecc8dc
Michał Brzuchalski <michal.brzuchalski@gmail.com>
2017-01-18 19:48:28 +0100

    Initial commit

---
+++ src/FilesystemStreamWrapper.php
@@ -0,0 +1,349 @@
+<?php declare(strict_types=1);
+
+/**
+ * Class FilesystemStreamWrapper
+ *
+ * This is onefile local filesystem streamWrapper class gives possibility
+ * to use normal file io built in functions.
+ * Docblock comments are missing deliberately due to pragmatic approach
+ *
+ * @use FilesystemStreamWrapper::register('app', __DIR__ . '/myapp_direcotry')
+ * @use FilesystemStreamWrapper::unregister('app')
+ * @see http://php.net/manual/en/class.streamwrapper.php
+ * @author Michał Brzuchalski <michal.brzuchalski@gmail.com>
+ */
+class FilesystemStreamWrapper
+{
+    private static $rootPaths = [];
+    public $context;
+    private $stream;
+    private $dir;
+
+    public static function register(string $protocol, string $rootPath)
+    {
+        self::$rootPaths[$protocol] = realpath($rootPath);
+        stream_wrapper_register($protocol, self::class);
+    }
+
+    public static function unregister(string $protocol)
+    {
+        stream_wrapper_unregister($protocol);
+    }
+
+    private static function resolve(string $path)
+    {
+        foreach (self::$rootPaths as $protocol => $rootPath) {
+            if (0 === strpos($path, "{$protocol}://")) {
+                break;
+            }
+        }
+        if (!empty($protocol) && array_key_exists($protocol, self::$rootPaths)) {
+            return self::$rootPaths[$protocol] . substr($path, strlen($protocol) + 2);
+        }
+        return $path;
+    }
+
+    public function dir_closedir() : bool
+    {
+        try {
+            if (is_resource($this->dir)) {
+                closedir($this->dir);
+            }
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+        return true;
+    }
+
+    public function dir_opendir(string $path , int $options) : bool
+    {
+        try {
+            $this->dir = opendir(self::resolve($path), $this->context);
+            return is_resource($this->dir);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    public function dir_readdir() : string
+    {
+        try {
+            if (is_resource($this->dir)) {
+                return (string)readdir($this->dir);
+            }
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+        }
+        return '';
+    }
+
+    public function dir_rewinddir() : bool
+    {
+        try {
+            if (is_resource($this->dir)) {
+                rewinddir($this->dir);
+            }
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+        return true;
+    }
+
+    public function mkdir(string $path , int $mode , int $options) : bool
+    {
+        try {
+            $resolved = self::resolve($path);
+            if (!@mkdir($resolved, $mode, ($options & STREAM_MKDIR_RECURSIVE) > 0, $this->context) && !is_dir($resolved)) {
+                throw new RuntimeException("Unable to create directory: {$path}");
+            }
+            return true;
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    public function rename(string $oldname, string $newname) : bool
+    {
+        try {
+            $resolved = self::resolve($oldname);
+            return file_exists($resolved) && rename($resolved, self::resolve($newname), $this->context);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    public function rmdir ( string $path , int $options ) : bool
+    {
+        try {
+            $resolved = self::resolve($path);
+            if ($options & STREAM_MKDIR_RECURSIVE) {
+                while (file_exists($resolved)) {
+                    if (!rmdir($resolved, $this->context)) {
+                        return false;
+                    }
+                    $resolved = dirname($resolved);
+                }
+                return true;
+            }
+            return file_exists($resolved) && rmdir($resolved, $this->context);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    /**
+     * @return resource
+     */
+    public function stream_cast(int $cast_as)
+    {
+        return $this->stream;
+    }
+
+    public function stream_close() : void
+    {
+        try {
+            fclose($this->stream);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+        }
+    }
+
+    public function stream_eof() : bool
+    {
+        try {
+            return is_resource($this->stream) && feof($this->stream);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    public function stream_flush() : bool
+    {
+        try {
+            return is_resource($this->stream) && fflush($this->stream);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    public function stream_lock(int $operation) : bool
+    {
+        try {
+            return is_resource($this->stream) && flock($this->stream, $operation);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+    /**
+     * @param   string   $path      The file path or URL to set metadata. Note that in the case of a URL,
+     *                              it must be a :// delimited URL. Other URL forms are not supported.
+     * @param   integer  $option    One of:
+     *                                  PHP_STREAM_META_TOUCH (The method was called in response to touch())
+     *                                  PHP_STREAM_META_OWNER_NAME (The method was called in response to chown() with string parameter)
+     *                                  PHP_STREAM_META_OWNER (The method was called in response to chown())
+     *                                  PHP_STREAM_META_GROUP_NAME (The method was called in response to chgrp())
+     *                                  PHP_STREAM_META_GROUP (The method was called in response to chgrp())
+     *                                  PHP_STREAM_META_ACCESS (The method was called in response to chmod())
+     * @param   integer  $value     If option is
+     *                                  PHP_STREAM_META_TOUCH: Array consisting of two arguments of the touch() function.
+     *                                  PHP_STREAM_META_OWNER_NAME or PHP_STREAM_META_GROUP_NAME: The name of the owner
+     *                                      user/group as string.
+     *                                  PHP_STREAM_META_OWNER or PHP_STREAM_META_GROUP: The value owner user/group argument as integer.
+     *                                  PHP_STREAM_META_ACCESS: The argument of the chmod() as integer.
+     * @return  boolean             Returns TRUE on success or FALSE on failure. If option is not implemented, FALSE should be returned.
+     */
+    public function stream_metadata(string $path ,int $option, $value) : bool
+    {
+        try {
+            $resolved = self::resolve($path);
+            switch ($option) {
+                case STREAM_META_TOUCH:
+                    $currentTime = time();
+                    return touch(
+                        $resolved,
+                        is_array($value) && array_key_exists(0, $value) ? $value[0] : $currentTime,
+                        is_array($value) && array_key_exists(1, $value) ? $value[1] : $currentTime
+                    );
+                case STREAM_META_OWNER_NAME:
+                    return chown($resolved, (string)$value);
+                case STREAM_META_OWNER:
+                    return chown($resolved, (int)$value);
+                case STREAM_META_GROUP_NAME:
+                    return chgrp($resolved, (string)$value);
+                case STREAM_META_GROUP:
+                    return chgrp($resolved, (int)$value);
+                case STREAM_META_ACCESS:
+                    return chmod($resolved, $value);
+                default:
+                    return false;
+            }
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    public function stream_open(string $path , string $mode , int $options , string &$opened_path = null) : bool
+    {
+        try {
+            return !empty($this->stream = fopen(self::resolve($path), $mode));
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    public function stream_read(int $count) : string
+    {
+        try {
+            if (is_resource($this->stream)) {
+                return fread($this->stream, $count);
+            }
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+        }
+        return '';
+    }
+
+    public function stream_seek(int $offset , int $whence = SEEK_SET) : bool
+    {
+        try {
+            return is_resource($this->stream) && fseek($this->stream, $offset, $whence);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    /**
+     * AFAIK There is no need for this on local filesystem streams
+     */
+    public function stream_set_option(int $option , int $arg1 , int $arg2) : bool
+    {
+        return false;
+    }
+
+    public function stream_stat() : array
+    {
+        try {
+            if (is_resource($this->stream)) {
+                return fstat($this->stream);
+            }
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+        }
+        return [];
+    }
+
+    public function stream_tell() : int
+    {
+        try {
+            if (is_resource($this->stream)) {
+                return ftell($this->stream);
+            }
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+        }
+        return 0;
+    }
+
+    public function stream_truncate(int $new_size) : bool
+    {
+        try {
+            return is_resource($this->stream) && ftruncate($this->stream, $new_size);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+
+    public function stream_write(string $data) : int
+    {
+        try {
+            if (is_resource($this->stream)) {
+                return fwrite($this->stream, $data);
+            }
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+        }
+        return 0;
+    }
+
+    public function unlink(string $path) : bool
+    {
+        try {
+            return unlink(self::resolve($path));
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+    /**
+     * @return array|bool fstat array, but if file doesn't exists it must return false
+     */
+    public function url_stat(string $path , int $flags)// : array
+    {
+        try {
+            $resolved = self::resolve($path);
+            if (!file_exists($resolved)) {
+                return false;
+            }
+            if (($flags & STREAM_URL_STAT_LINK) && is_link($resolved)) {
+                return stat(readlink($resolved));
+            }
+            return stat($resolved);
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return false;
+        }
+    }
+}
---
+++ tests/FilesystemStreamWrapperTest.php
@@ -0,0 +1,190 @@
+<?php
+
+class FilesystemStreamWrapperTest extends PHPUnit_Framework_TestCase
+{
+    private $testRootPath;
+
+    public function setUp()
+    {
+        $this->testRootPath = realpath(__DIR__ . '/../var/test');
+        if (!@mkdir($this->testRootPath) && !is_dir($this->testRootPath)) {
+            throw new RuntimeException("Unable to create test:// root path: {$this->testRootPath}");
+        }
+        FilesystemStreamWrapper::register('test', $this->testRootPath);
+    }
+
+    public function tearDown()
+    {
+        FilesystemStreamWrapper::unregister('test');
+        foreach (array_diff(scandir($this->testRootPath), array('.', '..')) as $path) {
+            if (is_dir($this->testRootPath . DIRECTORY_SEPARATOR . $path)) {
+                self::rmdirr($this->testRootPath . DIRECTORY_SEPARATOR . $path);
+            } else {
+                unlink($this->testRootPath . DIRECTORY_SEPARATOR . $path);
+            }
+        }
+    }
+
+    public static function rmdirr(string $dir) : bool
+    {
+        foreach (array_diff(scandir($dir), array('.', '..')) as $file) {
+            if (is_dir("$dir/$file")) {
+                self::rmdirr("$dir/$file");
+            } else {
+                unlink("$dir/$file");
+            }
+        }
+        return rmdir($dir);
+    }
+
+    public function testRegistration()
+    {
+        FilesystemStreamWrapper::register('test1', $this->testRootPath);
+        $this->assertTrue(in_array('test1', stream_get_wrappers()));
+        FilesystemStreamWrapper::register('test2', $this->testRootPath);
+        $this->assertTrue(in_array('test2', stream_get_wrappers()));
+
+        FilesystemStreamWrapper::unregister('test1');
+        $this->assertFalse(in_array('test1', stream_get_wrappers()));
+        FilesystemStreamWrapper::unregister('test2');
+        $this->assertFalse(in_array('test2', stream_get_wrappers()));
+    }
+
+//    public function testTouchRenameAndRemoveFile()
+//    {
+//        FilesystemStreamWrapper::register('test', $this->testRootPath);
+//
+//        $path = 'test://touch-and-remove.txt';
+//        $moved = 'test://touch-moved.txt';
+//        $this->assertFileNotExists($path);
+//        $this->assertTrue(touch($path));
+//        $this->assertFileExists($path);
+//        $this->assertTrue(rename($path, $moved));
+//        $this->assertFileNotExists($path);
+//        $this->assertFileExists($moved);
+//        $this->assertTrue(unlink($moved));
+//        $this->assertFileNotExists($moved);
+//
+//        FilesystemStreamWrapper::unregister('test');
+//    }
+//
+//    public function testMkdirMoveAndRmdir()
+//    {
+//        FilesystemStreamWrapper::register('test', $this->testRootPath);
+//
+//        $path = 'test://some-path/some-dir';
+//        $moved = 'test://some-path/new-dir';
+//        $this->assertDirectoryNotExists($path);
+//        $this->assertTrue(mkdir($path, 0777, true));
+//        $this->assertDirectoryExists($path);
+//        $this->assertTrue(rename($path, $moved));
+//        $this->assertDirectoryNotExists($path);
+//        $this->assertDirectoryExists($moved);
+//        $this->assertTrue(rmdir($moved));
+//        $this->assertDirectoryNotExists($moved);
+//
+//        FilesystemStreamWrapper::unregister('test');
+//    }
+//
+//    public function testDirectoryListing()
+//    {
+//        FilesystemStreamWrapper::register('test', $this->testRootPath);
+//
+//        touch('test://file1.txt');
+//        touch('test://file2.txt');
+//        mkdir('test://tmp');
+//        $directory = opendir('test://');
+//        $this->assertEquals('.', readdir($directory));
+//        $this->assertEquals('some-path', readdir($directory));
+//        $this->assertEquals('file1.txt', readdir($directory));
+//        $this->assertEquals('file2.txt', readdir($directory));
+//        $this->assertEquals('..', readdir($directory));
+//        $this->assertEquals('tmp', readdir($directory));
+//
+//        rewinddir($directory);
+//        $this->assertEquals('.', readdir($directory));
+//
+//        FilesystemStreamWrapper::unregister('test');
+//    }
+
+    /**
+     * Tests if the stream wrapper returns the expected result when reading a directory.
+     */
+    public function testDirectoryRead()
+    {
+        touch('test://file.txt');
+        mkdir('test://directory');
+        $dir = opendir('test://');
+        $entries = [];
+        while ($entry = readdir($dir)) {
+            $entries[] = $entry;
+        }
+        $this->assertSame(array('.', 'directory', '..', 'file.txt'), $entries);
+    }
+
+    /**
+     * Tests if the stream wrapper returns the expected result when rewinding a directory handle.
+     */
+    public function testDirectoryRewind()
+    {
+        touch('test://file.ext');
+        $directoryResource = opendir('test://');
+        $entry = readdir($directoryResource);
+        rewinddir($directoryResource);
+        $this->assertSame($entry, readdir($directoryResource));
+    }
+
+    /**
+     * Tests if the stream wrapper creates a directory.
+     */
+    public function testDirectoryCreateAndRemove()
+    {
+        $this->assertTrue(mkdir('test://directory/in-a-directory', 0777, true));
+        $this->assertTrue(rmdir('test://directory/in-a-directory'));
+    }
+
+    /**
+     * Tests if the stream wrapper touches a file.
+     */
+    public function testTouch()
+    {
+        mkdir('test://directory');
+        $this->assertTrue(touch('test://directory/with-a-file.ext'));
+        $this->assertFileExists($this->testRootPath . '/directory/with-a-file.ext');
+    }
+
+    /**
+     * Tests if the stream wrapper unlinks a file.
+     */
+    public function testUnlink()
+    {
+        mkdir('test://directory');
+        touch('test://directory/with-a-file.ext');
+        $this->assertTrue(unlink('test://directory/with-a-file.ext'));
+        $this->assertFileNotExists($this->testRootPath . '/directory/with-a-file.ext');
+    }
+
+    /**
+     * Tests if the stream wrapper writes to a file.
+     *
+     * @depends testUnlink
+     */
+    public function testReadAndWriteFile()
+    {
+        $this->assertSame(9, file_put_contents('test://file.ext', "contents\n"));
+        $this->assertFileExists($this->testRootPath . '/file.ext');
+        $this->assertSame("contents\n", file_get_contents('test://file.ext'));
+    }
+
+    /**
+     * Tests if the stream wrapper renames a file.
+     */
+    public function testRename()
+    {
+        touch('test://file.ext');
+        $this->assertFileExists($this->testRootPath . '/file.ext');
+        $this->assertTrue(rename('test://file.ext', 'test://file.ext2'));
+        $this->assertFileNotExists($this->testRootPath . '/file.ext');
+        $this->assertFileExists($this->testRootPath . '/file.ext2');
+    }
+}
